0ac0e89dea6744e577ff88cde5be86fc73924b83
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-02-25 22:07:01 +0300

    finished

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/current_time/")  # можно ли обойтись без этого декоратора и функции, чтобы настроить часы???
-def hello_world():
-    now = datetime.now()
-    current_time = now.strftime('%H:%M:%S')
-    return f'Текущее время {current_time}'
 
 @app.route("/")
-def clock():
-    return render_template("index.html")
+def home():
+    context = {
+        'M10': 'Messi',
+        'CR7': 'Ronaldo',
+        'Sheva': 'Shevchenko'
+    }
+    return render_template("home.html", **context)
 
-@app.route("/blog")
-def blog():
-    return render_template("blog.html")
 
-@app.route("/contacts")
+@app.route("/about")
 def contacts():
-    return render_template("contacts.html")
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
